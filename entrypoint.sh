0a704872284444fddcca6edb9b5cd11e396c8426
Alexander Trost <galexrt@googlemail.com>
2017-11-30 16:35:32 +0100

    Fixed some issues in the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,10 @@ RESTIC_PASSWORD="${RESTIC_PASSWORD:-}"
 RESTIC_PASSWORD_FILE="${RESTIC_PASSWORD_FILE:-}"
 RESTIC_FORGET_FLAGS="${RESTIC_FORGET_FLAGS:-}"
 
-SSH_CONFIG_FILE="${SSH_CONFIG_FILE:-/dev/null}"
-
 BACKUP_TARGET="${BACKUP_TARGET:-/target}"
 
 if [ ! -z "$RESTIC_FORGET_FLAGS" ]; then
-    restic forget "$RESTIC_FORGET_FLAGS"
+    restic forget $RESTIC_FORGET_FLAGS
 fi
-
-rm -f /etc/ssh/ssh_config
-ln -s "$SSH_CONFIG_FILE" /etc/ssh/ssh_config
 
 exec restic backup "$BACKUP_TARGET"
