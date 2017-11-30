90829ce3305074073409c7bd8f3ccf9b273b38af
Alexander Trost <galexrt@googlemail.com>
2017-11-30 11:25:46 +0100

    Added env var for ssh_config file location

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM debian:jessie
 
 RUN apt-get -q update && \
     apt-get upgrade -y && \
-    apt-get install -y sshfs curl && \
+    apt-get install -y curl && \
     curl -L https://github.com/restic/restic/releases/download/v0.8.0/restic_0.8.0_linux_amd64.bz2 -o /usr/local/bin/restic && \
-    mkdir -p /destination
+    apt-get --purge remove -y curl
 
 ADD entrypoint.sh /usr/local/bin
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,15 @@ RESTIC_PASSWORD="${RESTIC_PASSWORD:-}"
 RESTIC_PASSWORD_FILE="${RESTIC_PASSWORD_FILE:-}"
 RESTIC_FORGET_FLAGS="${RESTIC_FORGET_FLAGS:-}"
 
+SSH_CONFIG_FILE="${SSH_CONFIG_FILE:-/dev/null}"
+
 BACKUP_TARGET="${BACKUP_TARGET:-/target}"
 
 if [ ! -z "$RESTIC_FORGET_FLAGS" ]; then
     restic forget "$RESTIC_FORGET_FLAGS"
 fi
 
-restic backup "$BACKUP_TARGET"
+rm -f /etc/ssh/ssh_config
+ln -s "$SSH_CONFIG_FILE" /etc/ssh/ssh_config
+
+exec restic backup "$BACKUP_TARGET"
