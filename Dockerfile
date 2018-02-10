fa132b47f845587d39745a574c6dbfeb16961588
Alexander Trost <galexrt@googlemail.com>
2018-02-10 16:34:17 +0100

    Added Prometheus Metrics

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,12 @@ RUN apt-get -q update && \
     curl -L https://github.com/restic/restic/releases/download/v0.8.1/restic_0.8.1_linux_amd64.bz2 -o /usr/local/bin/restic.bz2 && \
     bzip2 -d /usr/local/bin/restic.bz2 && \
     chmod 755 /usr/local/bin/restic && \
+    mkdir -p /var/lib/node_exporter && \
     apt-get --purge remove -y curl && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+VOLUME ["/var/lib/node_exporter"]
 
 ADD entrypoint.sh /usr/local/bin
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,9 @@ IONICE_CLASSDATA="${IONICE_CLASSDATA:-7}"
 
 BACKUP_TARGET="${BACKUP_TARGET:-/target}"
 
+PROMETHEUS_METRICS=${PROMETHEUS_METRICS:-true}
+PROMETHEUS_METRICS_PATH="${PROMETHEUS_METRICS_PATH:-/var/lib/node_exporter/backup-restic-sftp.prom}"
+
 if [ ! -z "$RESTIC_FORGET_FLAGS" ]; then
     echo "-> Running 'restic forget $RESTIC_FORGET_FLAGS' ..."
     restic forget --host "$RESTIC_HOSTNAME" $RESTIC_FORGET_FLAGS
@@ -20,4 +23,18 @@ restic snapshots --host "$RESTIC_HOSTNAME"
 echo "==="
 
 echo "-> Running 'restic backup $BACKUP_TARGET' ..."
-exec ionice -c "$IONICE_CLASS" -n "$IONICE_CLASSDATA" restic backup --hostname "$RESTIC_HOSTNAME" "$BACKUP_TARGET"
+ionice -c "$IONICE_CLASS" -n "$IONICE_CLASSDATA" restic backup --hostname "$RESTIC_HOSTNAME" "$BACKUP_TARGET"
+
+SFTP_SERVER="$(echo "$RESTIC_REPOSITORY" | cut -d':' -f2)"
+SFTP_DF_OUTPUT="$(echo "df" | sftp "${SFTP_SERVER}" | tail -n1)"
+
+if [ $PROMETHEUS_METRICS ]; then
+    cat <<EOF > "$PROMETHEUS_METRICS_PATH"
+# HELP sftp_backup_space_size SFTP backup space size in bytes.
+# TYPE sftp_backup_space_size gauge
+sftp_backup_space_size{server="${SFTP_SERVER}"} $(echo "${SFTP_DF_OUTPUT}" | awk '{print $1}')
+# HELP sftp_backup_space_used SFTP backup space used in bytes.
+# TYPE sftp_backup_space_used gauge
+sftp_backup_space_used{server="${SFTP_SERVER}"} $(echo "${SFTP_DF_OUTPUT}" | awk '{print $2}')
+EOF
+fi
